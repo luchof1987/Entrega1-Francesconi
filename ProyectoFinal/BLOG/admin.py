98f0f54c0441449fe7e65c981371c7611ab64506
luchof1987 <114319120+luchof1987@users.noreply.github.com>
2022-10-05 11:00:27 -0300

    commit

--- ProyectoFinal/BLOG/admin.py
+++ ProyectoFinal/BLOG/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from BLOG.models import Autor, Articulo, Seccion
 
 # Register your models here.
-admin.site.register
+admin.site.register(Articulo)
+admin.site.register(Autor)
+admin.site.register(Seccion)
--- ProyectoFinal/BLOG/models.py
+++ ProyectoFinal/BLOG/models.py
@@ -1,18 +1,29 @@
 from django.db import models
 
-class Autor(models.Model):
-    nombre=models.CharField(max_length=100)
-    apellido=models.CharField(max_length=100)
-    profesion=models.CharField(max_length=100)
 
+class Autor(models.Model):
+
+    class Meta:
+        verbose_name_plural="Autores"
+
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    profesion = models.CharField(max_length=100)
 
 
 class Articulo(models.Model):
-    titulo=models.CharField(max_length=100)
-    texto=models.CharField(max_length=1000)
-    fecha=models.DateField(null=True)
 
+    class Meta:
+        verbose_name_plural="Articulos"
+
+    titulo = models.CharField(max_length=100)
+    texto = models.CharField(max_length=1000)
+    fecha = models.DateField(null=True)
 
 
 class Seccion(models.Model):
-    nombre=models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural="Secciones"
+
+    nombre = models.CharField(max_length=100)
